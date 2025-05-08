0a84deee0ad251484599dc6c6decdc52fbf6f376
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-08 15:29:54 +0200

    people function

--- names.js
+++ names.js
@@ -2,5 +2,6 @@ const completeNameObject = (firstName, LastName) =>{
     const completeName = {firstName, LastName};
     return completeName;
 };
+
 module.exports = completeNameObject;
 
--- people.js
+++ people.js
@@ -1,5 +1,13 @@
 const completeNameObject = require("./names.js");
 const createHobbies = require("./hobbies.js");
 
+const createPeople = () =>{
+    const people = {
+        fullName : completeNameObject("Andrea", "moscatelli"),
+        hobbies : createHobbies("studiare", "correre", "leggere")
+    };
+    return people;
+};
 
+console.log(createPeople());
 
